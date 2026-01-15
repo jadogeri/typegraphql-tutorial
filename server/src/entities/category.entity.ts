ca5dbae73d921a9a5221de8b200c367e79a94027
josephadogeriopao <joseph.adogeri@orleansassessors.com>
2026-01-15 11:12:59 -0600

    Implemented Category class

--- server/src/entities/category.entity.ts
+++ server/src/entities/category.entity.ts
@@ -9,6 +9,7 @@ import { IsNumber, IsString } from "class-validator";
 import { ObjectType, Field, ID } from "type-graphql";
 import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm";
 
+
 @ObjectType()
 @Entity()
 export class Category extends BaseEntity { // BaseEntity provides static methods like .find(), .save() etc.
