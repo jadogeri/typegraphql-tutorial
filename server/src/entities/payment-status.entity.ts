508da62fa0dd7b1bfdf71c6fa585ecdd23dfd8e0
josephadogeriopao <joseph.adogeri@orleansassessors.com>
2026-01-15 11:27:08 -0600

    Implemented Payment Status class

---
+++ server/src/entities/payment-status.entity.ts
@@ -0,0 +1,26 @@
+/**
+ * @author Joseph Adogeri
+ * @version 1.0.0
+ * @since 15-JAN-2026
+ * @filename payment-status.entity.ts
+ * @file Payment Status Entity Definition using TypeORM and TypeGraphQL
+ */
+import { IsNumber, IsString } from "class-validator";
+import { ObjectType, Field, ID } from "type-graphql";
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm";
+
+
+@ObjectType()
+@Entity()
+export class PaymentStatus extends BaseEntity { // BaseEntity provides static methods like .find(), .save() etc.
+  @Field(() => ID) // Exposes 'id' to GraphQL as an ID type
+  @PrimaryGeneratedColumn() // TypeORM primary key
+  @IsNumber() // Validation to ensure it's a number
+  id!: number;
+
+  @Field(() => String) // Exposes 'name' to GraphQL
+  @Column({ type: "varchar", nullable: false, unique: true }) // TypeORM column
+  @IsString() // Validation to ensure it's a string
+  name!: string;
+
+}
