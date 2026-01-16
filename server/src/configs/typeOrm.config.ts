ce557a8abd4ca355c448c8b49bc3b6acc35f5d15
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-01-16 06:31:31 -0600

    updated data source node environment

--- server/src/configs/typeOrm.config.ts
+++ server/src/configs/typeOrm.config.ts
@@ -17,16 +17,16 @@ import { SeederOptions } from "typeorm-extension";
 import { NodeEnvironment } from "../types/node-environment.type.js";
 
 const options: DataSourceOptions & SeederOptions = {
-      type: process.env.NODE_ENV === "production" ? "sqlite" : "better-sqlite3", // or "mysql", "sqlite", etc.
+    type: process.env.NODE_ENV === "production" ? "sqlite" : "better-sqlite3", // or "mysql", "sqlite", etc.
     driver: process.env.NODE_ENV === "production" ? require("@libsql/sqlite3") : null,
     flags: process.env.NODE_ENV === "production" ? 0x00000040 : undefined, // required for TypeORM with libsql
-    database: process.env.NODE_ENV === "production" ? 
-      process.env.TURSO_DATABASE_URL + "?authToken=" + process.env.TURSO_AUTH_TOKEN || process.env.PROD_DATABASE_URL : 
-      process.env.DEV_DATABASE_URL || "userDB.sqlite",
+    database: process.env.NODE_ENV === "test" || process.env.NODE_ENV === "staging"|| process.env.NODE_ENV === "development" ? 
+      process.env.DEV_DATABASE_URL || "userDB.sqlite" :   
+      process.env.TURSO_DATABASE_URL + "?authToken=" + process.env.TURSO_AUTH_TOKEN || process.env.PROD_DATABASE_URL ,
     synchronize: process.env.NODE_ENV !== "production" ,
     logging: false,
     entities: [Region, Category, PaymentMethod, PaymentStatus, OrderStatus, Order, Invoice ], // List your entities here
-     migrations: ["src/migrations/**/*.ts"],
+    migrations: ["src/migrations/**/*.ts"],
     subscribers: [],
   
 }
