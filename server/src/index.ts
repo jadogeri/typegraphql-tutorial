ffb8db0f9b8c7e45552a453c8c94a735e17f3e93
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-01-15 23:05:29 -0600

    removed comments

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,8 +4,6 @@ import app from './app';
 
 const PORT = 3000;
 
-// await connect(); // Ensure database connection is established
-
 // Start the server
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on http://localhost:${PORT}`);
