83d73f09a7e5dbad3564b20110ce6da5fdc9f01d
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-01-16 23:50:35 -0600

    Added category service interface

---
+++ server/src/interfaces/category-service.interface.ts
@@ -0,0 +1,9 @@
+import { Category } from "../entities/category.entity";
+
+export interface CategoryServiceInterface {
+    getAllCategories(): Promise<Category[]>;
+    getCategoryById(id: number): Promise<Category | null>;
+    createCategory(name: string): Promise<Category>;
+    updateCategory(id: number, name: string): Promise<Category | null>;
+    deleteCategory(id: number): Promise<boolean>;
+}
