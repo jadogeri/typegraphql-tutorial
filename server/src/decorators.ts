3edd5b451cefb34124d68af61764d2c969fe7c8e
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-01-17 00:00:02 -0600

    Added decorators

---
+++ server/src/decorators.ts
@@ -0,0 +1,14 @@
+import { injectable, inject } from "inversify";
+import { Middlewares } from "tsoa";
+
+const Service = injectable;
+
+const Repository = injectable;
+
+const Controller = injectable;
+
+const AutoWired = inject;
+
+const Middleware = Middlewares
+
+export { Service, Repository, Controller, Middleware, AutoWired  };
