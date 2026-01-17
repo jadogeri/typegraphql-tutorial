cfebd63a49124978fc5a791b203eadfa15d7c772
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-01-17 00:37:08 -0600

    Added authentication middleware boilerplate code

---
+++ server/src/middlewares/authentication.middleware.ts
@@ -0,0 +1,13 @@
+// src/authentication.ts
+import {Request} from 'express';
+
+
+export async function expressAuthentication(
+  request: Request,
+  securityName: string,
+  scopes?: any[]
+): Promise<any> {
+
+  return await Promise.resolve({ userId: 1, username: 'testuser' });
+
+}
