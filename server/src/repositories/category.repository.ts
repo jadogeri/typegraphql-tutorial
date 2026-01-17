58be63517145056d39d2480c860617f98ef9a0e4
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-01-17 00:19:18 -0600

    Added template for Category repository

---
+++ server/src/repositories/category.repository.ts
@@ -0,0 +1,41 @@
+import { DataSource, Repository as BaseRepository } from "typeorm";
+import { Repository } from "../decorators";
+import { inject } from "inversify";
+import { TYPES } from "../types/binding.type";
+import { Category } from "../entities/category.entity";
+import { CategoryRepositoryInterface } from "../interfaces/category-repository.interface";
+
+    
+@Repository()
+export class CategoryRepository extends BaseRepository<Category> implements CategoryRepositoryInterface {
+    
+
+    constructor(@inject(TYPES.DataSource) dataSource: DataSource) {
+        //console.log("Registered Entities:", dataSource.entityMetadatas.map(m => m.name));
+        super(Category, dataSource.createEntityManager());
+        
+    }
+    getAll(): Promise<Category[]> {
+        throw new Error("Method not implemented.");
+    }
+    getOne(id: number): Promise<Category | null> {
+        throw new Error("Method not implemented.");
+    }
+    getCategory(): any {
+        return {message: "Get category endpoint from repository" };
+    }
+    createCategory  (): any {
+        return {message: "Create category endpoint from repository" };
+    }
+    updateCategory(): any {
+        return {message: "Update category endpoint from repository" };
+    }
+    replaceCategory(): any {
+        return {message: "Replace category endpoint from repository" };
+    }
+    deleteCategory(): any {
+        return {message: "Delete category endpoint from repository" };
+    }  
+    
+}
+
