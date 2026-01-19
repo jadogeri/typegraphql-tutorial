ee8a12e74e14136778dd3f70affb02da18ca83ba
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-01-19 13:42:32 -0600

    Removed routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -38,18 +38,6 @@ export const buildApp = (): Application => {
     res.json({ message: 'get received successfully' });
   });
 
-  app.post('/', (req: Request, res: Response) => {
-    res.json({ message: 'post received successfully' });
-  });
-
-  app.delete('/', (req: Request, res: Response) => {
-    res.json({ message: 'delete received successfully' });
-  });
-
-  app.put('/', (req: Request, res: Response) => {
-    res.json({ message: 'put received successfully' });
-  });
-
   app.use(["/openapi", "/docs", "/swagger"],
     swaggerUI.serve,
     swaggerUI.setup(swaggerJson, swaggerOptions)
