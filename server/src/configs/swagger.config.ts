845f8265a28e3075980fd4e70f946e5ac599f5b9
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-01-18 21:06:25 -0600

    resolved swagger options

--- server/src/configs/swagger.config.ts
+++ server/src/configs/swagger.config.ts
@@ -1,18 +1,19 @@
+import { SwaggerUiOptions } from "swagger-ui-express";
 
+// CORRECT: Include the full path and a specific version
 const SWAGGER_URL = "https://cdnjs.cloudflare.com";
-const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
-  
 
-// Define the custom options
-const swaggerOptions = {
-  //customCssUrl: `${SWAGGER_URL}/swagger-ui.min.css`,
-  customCssUrl: CSS_URL,
+const swaggerOptions: SwaggerUiOptions = {
+  // Correctly resolves to: https://cdnjs.cloudflare.com/swagger-ui.min.css
+  customCssUrl: `${SWAGGER_URL}/swagger-ui.min.css`,
   customJs: [
     `${SWAGGER_URL}/swagger-ui-bundle.min.js`,
     `${SWAGGER_URL}/swagger-ui-standalone-preset.min.js`
   ],
   swaggerOptions: {
+    // Required to tell the UI where your generated JSON lives
     url: "/swagger.json", 
+    deepLinking: true,
   },
   customSiteTitle: "My API Docs",
 };
